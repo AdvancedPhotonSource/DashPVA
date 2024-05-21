6a056a30bfaa2d5c634e5f0a46f8e4fe90554e5d
Julio Rodriguez <julio.rodriguez@anl.gov>
2024-05-21 13:47:02 -0500

    fixed input channel in hpc collector

--- gui/start_HPC_Collector.py
+++ gui/start_HPC_Collector.py
@@ -10,7 +10,7 @@ def main():
     with open("gui/PVs.json", "r") as json_file:
         metadata = json.load(json_file)
 
-    command = "pvapy-hpc-collector --collector-id 1 --producer-id-list 1 --input-channel pvapy:"+prefix+":Pva1:Image --control-channel collector:*:control"
+    command = "pvapy-hpc-collector --collector-id 1 --producer-id-list 1 --input-channel "+prefix+":Pva1:Image --control-channel collector:*:control"
     command += " --status-channel collector:*:status --output-channel collector:*:output --processor-file hpcexample.py --report-period 5"
     command += " --server-queue-size 100 --collector-cache-size 100 --monitor-queue-size 1000"
 
