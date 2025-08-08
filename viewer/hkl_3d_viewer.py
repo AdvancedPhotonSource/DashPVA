74b74d17d84353707e0314c0a9968edd0f14d4c2
Osayi-ANL <oodiaseigiehon@anl.gov>
2025-08-08 16:49:48 -0500

    Updating Remote update: HKL 3D Slice, - slice a 3d mesh - see an interpolated plane of the data - add camera controls - load an h5 file into 3d points - made a function for creating/updating the 3d plot
    HKL 3D Slice UI,
    - add camera controls

--- viewer/hkl_3d_viewer.py
+++ viewer/hkl_3d_viewer.py
@@ -1,3 +1,4 @@
+from stat import filemode
 import sys, pathlib
 import subprocess
 import numpy as np
@@ -15,7 +16,12 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QFileDialog
 from pva_reader import PVAReader
 # Add the parent directory to the path so the font_scaling.py file can be imported
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
-from font_scaling import GlobalFontScaling
+from size_manager import SizeManager
+
+import h5py
+import numpy as np
+import os
+import time
 
 class ConfigDialog(QDialog):
 
@@ -397,11 +403,22 @@ class HKLImageWindow(QMainWindow):
 
 
 class HKL3DSliceWindow(QMainWindow):
+    # Todo: 
+    # Fix wierd glitch with sphere in arrow after update
+    # The interpolated plane mbecomes movable when data is loaded please fixs
+    # Color map is a little wierd for the intensity -- make it a different color
+    # HKL axis not bound to data -- bound to the bounds of the grid
+    # Slice gives error and disappears when out of bounds
+    # Lut and Opacity problem -- when they are applied interpolation disappears
+    # The reason the 3DSlicer causes a segmentation fault is because the widgets inside them -- have to close saftley
+    #   -- Reference AnalysisWindow.closeEvent()
+    # (Done) Load the data from the an hd5 file 
+
     def __init__(self, parent):
         """Initializes the viewing window for the 3D slicer"""
 
 
-        # Initialize the window
+        # -------- Initialize the window
         super(HKL3DSliceWindow, self).__init__()
         self.parent = parent
         uic.loadUi('gui/hkl_3d_slice_window.ui', self)
@@ -409,99 +426,329 @@ class HKL3DSliceWindow(QMainWindow):
         pyv.set_plot_theme('dark')
 
 
-        # Connecting the signals to the code that will be executed
+        # ------ Connecting the signals to the code that will be executed
         self.btnZoomIn.clicked.connect(self.zoom_in)
         self.btnZoomOut.clicked.connect(self.zoom_out)
+        self.btnResetCamera.clicked.connect(self.reset_camera)
+        self.btnSetCamPos.clicked.connect(self.set_camera_position)
+
+        self.btnLoadHD5.clicked.connect(self.load_data)
         
 
-        # Retrieve data from parent
+        # ------ Retrieve data from parent ----- (Initially set to copy the data from the parent)
+        # Change to create the points instead of copying so that there is no need to plot
         self.cloud_copy = self.parent.cloud.copy(deep=True)
         self.cloud_copy['intensity'] = self.parent.cloud['intensity']
 
 
-        # Creating the lookup table
-        self.lut = pyv.LookupTable(cmap='viridis')  
-        self.lut.below_range_color = 'black'
-        self.lut.above_range_color = 'black'
-        self.lut.below_range_opacity = 0
-        self.lut.above_range_opacity = 0 
-        self.lut.apply_opacity([0,1]) # intensity change to Julio
+        # ------- Look up table & Opacity
+        self.lut = pyv.LookupTable(cmap='magma')  
+        min_intensity = np.max(self.cloud_copy['intensity']) / 2
+        # May not need to do these portions
+        max_intensity = np.max(self.cloud_copy['intensity'])
+        masked_scalar = np.copy(self.cloud_copy['intensity']).astype(float)
+        masked_scalar[masked_scalar < min_intensity] = np.nan
+        masked_scalar[masked_scalar > max_intensity] = np.nan
+        self.cloud_copy['intensity'] = masked_scalar
+
+        self.lut.scalar_range = (
+            min_intensity, 
+            np.nanmax(self.cloud_copy['intensity'])
+        )
+
+        self.cloud_copy = self.cloud_copy.threshold([min_intensity,max_intensity])
 
 
-        # Creating the plotter for the 3d slicer
+        # ------- Creating the plotter for the 3d slicer
         self.plotter = QtInteractor()
-        # self.grid = self.cloud_copy.cast_to_unstructured_grid()
-        vol = self.cloud_copy.cast_to_unstructured_grid()
+        self.create_3D(cloud=self.cloud_copy.points, intensity=self.cloud_copy['intensity'], num_of_img=len(self.parent.reader.cache_images))
+        self.viewer_3d_slicer_layout.addWidget(self.plotter,1,1)
+        # -------------------------------------------------------------------- #
 
-        sphere = pyv.Sphere(radius=1.0)
-        interpolated = self.cloud_copy.sample(sphere)
-        # interpolated['intensity'] = interpolated['intensity'] * 1000000
+
+    # ================ METHODS ======================= #
+    def lower_res(self, factor:int=2) -> None:
+        """ 
+        Lower the resolution of an image
+
+        Args:: 
+        How It Works::
+        """
+        start_time = time.time()
+        # Get the shape and set the variables 
+        factor = factor
+        #! Reshape here by getting the dims
+        # H, W, D = self.cloud_copy.points
+        # Create the bins
+        # bin_w = None // factor
+        # bin_h = None // factor
+        # Trim the original image res to a factor of the res
+        # trim_w = bin_w * factor
+        # trim_h = bin_h * factor
+        # trim_orig = None
+        # Mask values using np.mask to prevent unwanted values from being averaging
+        # target_img = None
+        # downsampled_image = target_img.mean(axis=(1, 3))
+        end_time = time.time()
+        
+        # create the new points
+        # Set the new poly data to the new res 
+        print(f'\
+            Time: {(end_time-start_time).real} \n\
+            Factor: {factor} \n\
+            Original Points: {self.cloud_copy.points[:2]} \n\
+        ')
+
+
+    # Callback for updating the size of the points
+    def update_point_size(self, size):
+        actor = self.plotter.renderer._actors["points"]
+        actor.GetProperty().SetPointSize(size)
+
+
+    # Callback for updating the slice and vector visuals
+    def update_slice_from_plane(self, normal, origin):
+        # Todo: Doesn't interpolate only overlays
+        # Update slice
+        new_slice = self.vol.slice(normal=normal, origin=origin)
+        self.plotter.add_mesh(new_slice, scalars="intensity", cmap=self.lut, name="slice", show_edges=False, reset_camera=False)
+
+        # Remove previous origin and line
+        self.plotter.remove_actor("origin_sphere", reset_camera=False)
+        self.plotter.remove_actor("normal_line", reset_camera=False)
+
+        # Create new visuals
+        new_sphere = pyv.Sphere(radius=0.3, center=origin)
+        new_line = pyv.Line(pointa=origin, pointb=np.array(origin) + np.array(normal) * 2)
+
+        self.plotter.add_mesh(new_sphere, color='red', name="origin_sphere", pickable=False)
+        self.plotter.add_mesh(new_line, color='green', name="normal_line", pickable=False)
+
+        # Set their visibility based on toggle state
+        self.plotter.renderer._actors["origin_sphere"].SetVisibility(self.toggle_state["visible"])
+        self.plotter.renderer._actors["normal_line"].SetVisibility(self.toggle_state["visible"])
+
+
+    def load_data(self):
+        # ToDo: When trying to load from the application it does not load
+        file_name, _ = QFileDialog.getOpenFileName(self, 'Select an HD5 File')
+        if file_name:
+            self.lineEditFileLocation.setText(file_name)
+        # Temp fix
+        target = 'DashPVA'
+        parts = file_name.split(os.sep)
+        new_file = ''
+        if target in parts:
+            idx = parts.index(target)
+            sub_path = os.sep.join(parts[idx+1:])
+
+            new_file = os.path.join("..", sub_path)
+
+        result = self.load_h5_to_3d()
+        self.create_3D(cloud=result[0], intensity=result[1], num_of_img=result[2])
+
+
+    def create_3D(self, cloud=None, intensity=None ,num_of_img=0):
+        self.plotter.clear()
+        # Check if there is cloud data present if not 
+        # set the cloud to copy the parents image
+        if not np.all(cloud):
+            # need to create the 3d cloud in this case or call 
+            # the update function in the parent
+            cloud = self.cloud_copy
+            num_of_img = self.cloud_copy.shape[0]
+            intensity = self.parent.cloud['intensity']
+            print(f'\n\nNo cloud data present, using parent cloud data: {num_of_img} images\n\n')
+        
+        
+        self.groupBox3DViewer.setTitle(f'Viewing {num_of_img} Image(s)')
+        self.cloud_copy = pyv.PolyData(cloud)
+        self.cloud_copy['intensity'] = intensity
+
+        # ------- Lower the resolution ------- #
+        self.lower_res()
+
+
+        # ------- Look up table & Opacity ---- #
+        min_intensity = np.max(self.cloud_copy['intensity']) / 4
+        # May not need to do these portions
+        max_intensity = np.max(self.cloud_copy['intensity'])
+        masked_scalar = np.copy(self.cloud_copy['intensity']).astype(float)
+        masked_scalar[masked_scalar < min_intensity] = np.nan
+        masked_scalar[masked_scalar > max_intensity] = np.nan
+        self.cloud_copy['intensity'] = masked_scalar
+
+        self.lut.scalar_range = (
+            min_intensity, 
+            np.nanmax(self.cloud_copy['intensity'])
+        )
+
+        self.cloud_copy = self.cloud_copy.threshold([min_intensity,max_intensity])
+
+        #self.lut.apply_opacity([0.0,1.0])
         
 
-
+        # ----- Adding the point cloud data ----- #
         self.plotter.add_mesh(
-            vol,
+            self.cloud_copy,
             scalars="intensity",
-            cmap= self.lut,  # set flat intesnity
-            point_size=5.0,
-            render_points_as_spheres=True,
-            name="points"
+            cmap=self.lut,
+            point_size=6.0,
+            name="points",
+            reset_camera=False,
+            nan_color=None,
+            nan_opacity=0.0
         )
         
-
-        # Set origin and normal for the plane
-        init_origin = self.cloud_copy.center
-        init_norm = (1,1,0)
-        init_slice = interpolated.slice(normal=init_norm, origin=init_origin)
-        self.plotter.add_mesh(init_slice, cmap="jet", name="slice", show_edges=False)
+        # -------- Reset Camera Focus & Plotter Bounds ----- #
+        self.plotter.set_focus(self.cloud_copy.center)
+        self.plotter.reset_camera()
 
 
-        # Adding the sphere and line to the plotter
-        sphere = pyv.Sphere(radius=0.1, center=init_origin)
+        # ------- Adding the initial slice -------- #
+        bounds = None
+        spacing = None
+        dimensions = None
+        origin = self.cloud_copy.center
+        
+        self.grid = pyv.ImageData()
+        self.grid.spacing = (.008,.008,.008)
+        self.grid.dimensions = np.ceil((self.cloud_copy.points.max(axis=0) - self.cloud_copy.points.min(axis=0))*.5 / self.grid.spacing).astype(int) + 2
+        self.grid.origin = origin
+
+        self.vol = self.grid.interpolate(self.cloud_copy, radius=.5, sharpness=2)
+        self.plotter.show_bounds(mesh=self.cloud_copy, xtitle='H Axis', ytitle='K Axis', ztitle='L Axis', bounds=self.vol.bounds)
+
+        initial_normal = (0, 0, 1)
+        initial_origin = self.cloud_copy.center
+        initial_origin = np.array(self.vol.bounds[::2]) + (np.array(self.vol.bounds[1::2]) - np.array(self.vol.bounds[::2])) / 2
+        initial_slice = self.vol.slice(normal=initial_normal, origin=initial_origin)
+        self.plotter.add_mesh(initial_slice, scalars="intensity", name="slice", show_edges=False,reset_camera=False)
+
+
+        # ---- Add the interactive slicing plane widget
+        self.plotter.add_plane_widget(
+            callback=self.update_slice_from_plane,
+            normal=initial_normal,
+            origin=initial_origin,
+            bounds=self.vol.bounds,
+            factor=1.0,
+            implicit=True,
+            assign_to_axis=None,
+            tubing=False,
+            origin_translation=True
+        )
+
+
+        # -------- Adding the arrow -------------- #
+        sphere = pyv.Sphere(radius=0.3, center=initial_origin)
         line = pyv.Line(
-            pointa=init_origin,
-            pointb=np.array(init_origin) + np.array(init_norm) * 2
+            pointa=initial_origin,
+            pointb=np.array(initial_origin) + np.array(initial_normal) * 2
         )
         self.plotter.add_mesh(sphere, color='red', name="origin_sphere", pickable=False)
-        self.plotter.add_mesh(line, color='white', name="normal_line", pickable=False)
+        self.plotter.add_mesh(line, color='green', name="normal_line", pickable=False)
+
+        # Hide them manually after adding
         self.plotter.renderer._actors["origin_sphere"].SetVisibility(False)
         self.plotter.renderer._actors["normal_line"].SetVisibility(False)
 
+        # Store toggle visibility state
         self.toggle_state = {"visible": False}
 
 
-        self.viewer_3d_slicer_layout.addWidget(self.plotter)
-
-
-        #Errors encountered: 
-        # Reason: Whenever you start the live view, but don't plot before opening the 3d viewer
-        # Error: PyVistaPipelineError('The passed algorithm is failing to produce an output.')
-
-
-    def add_plane():
-        pass
-
-
-    def update_slice_from_plane(self):
-        print('normal and origin')
-        
-
-    def apply_clipping(self, normal):
-        """
-        Updates the clipped mesh of the plane
-        """
-        clipped_mesh = self.cloud_copy
-        self.plotter.clear()
-        self.plotter.add_mesh(clipped_mesh, name="clipped_hkl")
-        self.plotter.add_mesh_clip_plane(
-            mesh = clipped_mesh,
-            normal=normal
+        # ------- Point size slider ------------ #
+        self.plotter.add_slider_widget(
+            self.update_point_size,
+            rng=[1, 20],
+            value=5,
+            title="Point Size",
+            pointa=(0.01, .90),
+            pointb=(0.21, .90)
         )
 
 
+    # ---- Load From HD5 ---- #
+    def load_h5_to_3d(self, file:str='./6idxrayeye_scan.h5'):
+        """
+        Loads .h5 file to 3D points 
+
+        Args: 
+            file (str): String path to .h5 file
+
+        Returns: 
+            tuple: 
+                points - the 3D points to be plotted
+                flat_intensity - Intensity of the image
+                num_of_images - Number of images in the .h5 file
+            
+        Example: 
+            result = load_h5_to_3d('path/to/file')\n
+            pd = pyvista.PolyData(result[0])
+        """
+        
+        # Set the variables that will be returned
+        num_of_images = 0
+        points = None
+        flat_intensity = None
+        try:
+            with h5py.File(file, 'r') as f:
+
+                # q is for quaternion
+                # Grab the data needed in order to create 3D data
+                qx = f['entry/data/hkl/qx']
+                qy = f['entry/data/hkl/qy']
+                qz = f['entry/data/hkl/qz']
+                images = f['/entry/data/data']
+                num_of_images = f['entry/data/data'].shape[0]
+                
+                # Check that its not an empty images or data
+                if not qx or not qy or not qz:
+                    raise ValueError('No data/# of images is 0')
+                
+
+                # Turn the q's into a 1D array into 3D
+                # shape that can be concatenated store
+                # the data in a dict that can be accessed
+                q_dict = {}
+                for data in (qx, qy, qz):
+                    q_data_list = []
+                    
+                    # Reshape to a 1D array and store it in an array list
+                    for i in data:
+                        q_d = np.reshape(i, -1)
+                        q_data_list.append(q_d)
+                    
+                    # Create a numpy array of the list
+                    q_array = np.array(q_data_list)
+
+                    # Get the name of the vector being processed
+                    name = data.name.split('/')[-1]
+                    q_dict[name] = q_array 
+
+                # Reshape the number of images to a one dimensional image 
+                flat_intensity = np.reshape(images,-1)
+
+                # Concatenate each images and column stack in order to create the image in 3D
+                qx_con = np.concatenate(q_dict["qx"])
+                qy_con = np.concatenate(q_dict["qy"])
+                qz_con = np.concatenate(q_dict["qz"])
+                points = np.column_stack([
+                    qx_con, qy_con, qz_con
+                ])
+                return (points, flat_intensity, num_of_images)
+
+        # Prevent crashing by checking file exists, not empty, etc
+        # and return the empty points and 0 images
+        except Exception as e:
+            print('Error',e)
+            return (points, flat_intensity, num_of_images)
+              
+
+    # ---- Camera position ---- #
     def zoom_in(self):
         camera = self.plotter.camera
-        camera.zoom(-0.5)
+        camera.zoom(1.5)
         self.plotter.render()
 
 
@@ -511,9 +758,41 @@ class HKL3DSliceWindow(QMainWindow):
         self.plotter.render()
 
 
+    def reset_camera(self):
+        self.plotter.reset_camera()
+        self.plotter.render()
+
+
+    def set_camera_position(self):
+        pos = self.cbSetCamPos.currentText().lower()
+        if 'xy' in pos:
+            self.plotter.view_xy()   
+        elif 'yz' in pos:
+            self.plotter.view_yz()   
+        elif 'xz' in pos:
+            self.plotter.view_xz()   
+        self.plotter.render()
+
+
+    def closeEvent(self, event):
+        """
+        Handles cleanup operations when the HKL3DSliceWindow window is closed.
+
+        Args:
+            event (QCloseEvent): The close event triggered when the window is closed.
+        """
+        
+        del self.parent.slice_window
+        event.accept()
+        super(HKL3DSliceWindow, self).closeEvent(event)
+
+
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = ConfigDialog()
-    #GlobalFontScaling(app)
-    window.show()
-    sys.exit(app.exec_())
+    try:
+        app = QApplication(sys.argv)
+        window = ConfigDialog()
+        #SizeManager()
+        window.show()
+        sys.exit(app.exec_())
+    except KeyboardInterrupt:
+        sys.exit(0)
