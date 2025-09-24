6ed27872e69376b6b2e4aefb23e625c0a3d6e34e
Julio Rodriguez <julio.rodriguez@anl.gov>
2025-09-24 15:10:58 -0500

    fix: error in where the parent dir was appended to path

--- viewer/hkl_3d_viewer.py
+++ viewer/hkl_3d_viewer.py
@@ -14,9 +14,9 @@ from PyQt5 import uic
 from PyQt5.QtCore import QTimer, QThread, pyqtSignal, Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QFileDialog, QMessageBox
 # Custom imported classes
-from utils import PVAReader, HDF5Writer
 # Add the parent directory to the path so the font_scaling.py file can be imported
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
+from utils import PVAReader, HDF5Writer
 from utils import SizeManager
 from hkl_3d_slice_window import HKL3DSliceWindow
 
