21c6afee93b0802cbe2ae41cb8b9f4164f189e64
Osayi-ANL <oodiaseigiehon@anl.gov>
2026-01-08 10:05:04 -0600

    add: temp file to read data immediatly after saving and save compressed file

--- utils/hdf5_writer.py
+++ utils/hdf5_writer.py
@@ -46,14 +46,15 @@ class HDF5Writer(QObject):
 
             len_images = len(images)
             len_attributes = len(attributes)
+            print("Len of attr then images", len_attributes, len_images)
             
             if len(OUTPUT_FILE_CONFIG) == 2:
-                file_path = Path('~/DashPVA/test/').expanduser()
+                file_path = Path('~/hdf5/').expanduser()
                 if not file_path.exists():
                     file_path.mkdir(parents=True)
-                file_name = Path('mytemp')
+                file_name = Path('temp_hkl_3d.h5')
                 TEMP_FILE_LOCATION = file_path.joinpath(file_name)
-                
+
                 file_path = Path(OUTPUT_FILE_CONFIG['FilePath']).expanduser()
                 if not file_path.exists():
                     file_path.mkdir(parents=True)
@@ -80,8 +81,9 @@ class HDF5Writer(QObject):
             merged_metadata = {}
             for attribute_dict in attributes:
                 for key, value in attribute_dict.items():
-                    print(f"DICT ATTRS: {key} {value}")
                     if not (key == 'RSM' or key == 'Analysis'):
+                        # if type(value) == list:
+                        #     value = np.asarray(value)
                         if key not in merged_metadata:
                             merged_metadata[key] = []
                             merged_metadata[key].append(value)
@@ -98,7 +100,6 @@ class HDF5Writer(QObject):
                 motor_pos_grp = metadata_grp.create_group('motor_positions')
                 rois_grp = data_grp.create_group('rois')
                 for key, values in merged_metadata.items():
-                    print("Key",key,"Value",values)
                     if all(isinstance(v, (int, float, np.number)) for v in values):
                         if 'ROI' in key:
                             parts = key.split(':')
@@ -109,27 +110,55 @@ class HDF5Writer(QObject):
                         elif 'Position' in key:
                             motor_pos_grp.create_dataset(key, data=np.array(values))
                         else:
-                            metadata_grp.create_dataset(key, data=np.array(values))
+                            metadata_grp.create_dataset(key, data=np.array(values))      
                     elif all(isinstance(v, str) for v in values):
                         dt = h5py.string_dtype(encoding='utf-8')
                         metadata_grp.create_dataset(key, data=np.array(values, dtype=dt))
-
-            with h5py.File(TEMP_FILE_LOCATION, 'a') as h5f2:
+                    else:
+                        metadata_grp.create_dataset(key, data=np.array(np.reshape(values, -1)))
                 # Create HKL subgroup under images if HKL caches exist
-                if not compress:
+                
+            if not compress:
+                with h5py.File(TEMP_FILE_LOCATION, 'w') as h5f:
+                    # Create the main "images" group
+                    images_grp = h5f.create_group("entry")
+                    data_grp = images_grp.create_group('data')
+                    data_grp.create_dataset("data", data=np.array([np.reshape(img, shape) for img in images]), 
+                                        **hdf5plugin.Blosc(cname='lz4', clevel=5, shuffle=True))
+                    metadata_grp = data_grp.create_group("metadata")
+                    motor_pos_grp = metadata_grp.create_group('motor_positions')
+                    rois_grp = data_grp.create_group('rois')
+                    for key, values in merged_metadata.items():
+                        if all(isinstance(v, (int, float, np.number)) for v in values):
+                            if 'ROI' in key:
+                                parts = key.split(':')
+                                roi = parts[1]
+                                if roi not in rois_grp.keys():
+                                    rois_grp.create_group(name=roi)
+                                rois_grp[roi].create_dataset(key, data=np.array(values))
+                            elif 'Position' in key:
+                                motor_pos_grp.create_dataset(key, data=np.array(values))
+                            else:
+                                metadata_grp.create_dataset(key, data=np.array(values))      
+                        elif all(isinstance(v, str) for v in values):
+                            dt = h5py.string_dtype(encoding='utf-8')
+                            metadata_grp.create_dataset(key, data=np.array(values, dtype=dt))
+                        else:
+                            metadata_grp.create_dataset(key, data=np.array(np.reshape(values, -1)))
+
                     if HKL_IN_CONFIG and rsm:
-                        len_rsm = len(rsm[0])
-                        if rsm:
-                            if not (len_rsm == len_images):
-                                raise ValueError("[Saving Caches] qx, qy, and qz caches must have the same number of elements.")
-                            hkl_grp = data_grp.create_group(name="hkl")
-                            hkl_grp.create_dataset("qx", data=np.array([np.reshape(qx, shape) for qx in rsm[0]]), 
-                                                **hdf5plugin.Blosc(cname='lz4', clevel=5, shuffle=True))
-                            hkl_grp.create_dataset("qy", data=np.array([np.reshape(qy, shape) for qy in rsm[1]]), 
-                                                **hdf5plugin.Blosc(cname='lz4', clevel=5, shuffle=True))
-                            hkl_grp.create_dataset("qz", data=np.array([np.reshape(qz, shape) for qz in rsm[2]]), 
-                                                **hdf5plugin.Blosc(cname='lz4', clevel=5, shuffle=True))
-            print(f'HDF5 file save completed successfully: {OUTPUT_FILE_LOCATION}')
+                                len_rsm = len(rsm[0])
+                                if rsm:
+                                    if not (len_rsm == len_images):
+                                        raise ValueError("[Saving Caches] qx, qy, and qz caches must have the same number of elements.")
+                                    hkl_grp = data_grp.create_group(name="hkl")
+                                    hkl_grp.create_dataset("qx", data=np.array([np.reshape(qx, shape) for qx in rsm[0]]), 
+                                                        **hdf5plugin.Blosc(cname='lz4', clevel=5, shuffle=True))
+                                    hkl_grp.create_dataset("qy", data=np.array([np.reshape(qy, shape) for qy in rsm[1]]), 
+                                                        **hdf5plugin.Blosc(cname='lz4', clevel=5, shuffle=True))
+                                    hkl_grp.create_dataset("qz", data=np.array([np.reshape(qz, shape) for qz in rsm[2]]), 
+                                                        **hdf5plugin.Blosc(cname='lz4', clevel=5, shuffle=True))
+                print(f'HDF5 file save completed successfully: {OUTPUT_FILE_LOCATION} and temp {TEMP_FILE_LOCATION }')
             self.hdf5_writer_finished.emit(f"Caches successfully saved to {OUTPUT_FILE_LOCATION}")
         except Exception as e:
             self.hdf5_writer_finished.emit(f"Failed to save caches to {OUTPUT_FILE_LOCATION}: {e}")
